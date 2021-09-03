9c5f521e9de4b93393c6d58d8434e068a6554f09
LFallert <lfallert13@yahoo.com>
2021-09-02 20:08:00 -0500

    springboot exercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -7,74 +7,55 @@ import org.springframework.web.bind.annotation.*;
  * Created by Chris Bay
  */
 @Controller
-<<<<<<< HEAD
-=======
 @ResponseBody
-@RequestMapping("hello")
->>>>>>> origin/class-annotations
 public class HelloController {
 
-    //    // Handle request at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
+    @GetMapping()
+    public String reply(@RequestParam String n, @RequestParam String l) {
 
-<<<<<<< HEAD
-    @GetMapping("goodbye")
-    @ResponseBody
-=======
-    // lives at /hello/goodbye
-    @GetMapping("goodbye")
->>>>>>> origin/class-annotations
-    public String goodbye() {
-        return "Goodbye, Spring!";
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
+
     }
-
-    // Handles requests of the form /hello?name=LaunchCode
-<<<<<<< HEAD
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-=======
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
->>>>>>> origin/class-annotations
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
-
-    // Handles requests of the form /hello/LaunchCode
-<<<<<<< HEAD
-    @GetMapping("hello/{name}")
-    @ResponseBody
-=======
-    @GetMapping("{name}")
->>>>>>> origin/class-annotations
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
-
-<<<<<<< HEAD
 
     @GetMapping("form")
-    @ResponseBody
-    public String helloForm() {
-        return "<html>" +
+    public String hello() {
+        return  "<html>" +
                 "<body>" +
-                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
-=======
-    // /hello/form
-    @GetMapping("form")
-    public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
->>>>>>> origin/class-annotations
-                "<input type = 'text' name = 'name' >" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
+                "<form action='/hello' method='post'>" +
+                "<input type='text' name='name' />" +
+                "<select name='language'>" +
+                "<option value='english'>English</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='italian'>Italian</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet me!' />" +
                 "</form>" +
                 "</body>" +
-                "</html>";
+                "</html>"
+                ;
     }
+
+
+
 
 }
